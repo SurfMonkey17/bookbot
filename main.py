7a7f1d794ad6e94a66dce84e6ab9723614bd0fc0
SurfMonkey17 <sherryw17@gmail.com>
2024-12-31 08:53:20 -0600

    created word count

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    
+    words = file_contents.split()
+    word_count = len(words)   
+     
+    print(word_count)
+
+
+main()
